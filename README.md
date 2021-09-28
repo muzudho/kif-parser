accea2c889f15b70fdcf561bc07c6cb71087e115
むずでょ <muzudho1@gmail.com>
2021-09-28 20:39:03 +0900

    kifu-to-kif

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 WIP KIFパーサー
 
-## KIF を KIFU に変換
+## KIF から KIFU へ変換
 
 1. 📂`kif` に `*.kif` ファイルをたくさん入れてください
 2. ターミナルで `python.exe kif-to-kifu.py` コマンドを実行してください
 3. UTF-8形式に変換して 📂`kifu` ファイルに変換されます
 4. 読み終えた `kif/*.kif` ファイルは、 `kif-done/*.kif` へ移動されます
+
+## KIFU から KIF へ変換
+
+1. 📂`kifu` に `*.kifu` ファイルをたくさん入れてください
+2. ターミナルで `python.exe kifu-to-kif.py` コマンドを実行してください
+3. Shift-JIS形式に変換して 📂`kif` ファイルに変換されます
+4. 読み終えた `kifu/*.kifu` ファイルは、 `kifu-done/*.kifu` へ移動されます
--- kifu-to-kif.py
+++ kifu-to-kif.py
@@ -3,35 +3,35 @@ import shutil
 import os
 import codecs
 
-def convert_kif_file(path):
-    """(1) kif フォルダーの *.kifファイルを読み取ります
-    (2) *.kifuファイルを kifu フォルダーへ生成します
-    (3) 読み終えた *.kifファイルは kif-done フォルダーへ移動します
+def convert_kifu_file(path):
+    """(1) kifu フォルダーの *.kifuファイルを読み取ります
+    (2) *.kifファイルを kif フォルダーへ生成します
+    (3) 読み終えた *.kifuファイルは kifu-done フォルダーへ移動します
 
     Returns
     -------
     str
         新しいパス。
-        KIFファイルでなかったなら空文字列
+        KIFUファイルでなかったなら空文字列
     """
 
     newPath = ""
 
     # シフトJISエンコードのテキストファイルの読み込み
-    with codecs.open(path, "r", encoding='shift_jis') as f:
+    with codecs.open(path, "r", encoding='utf-8') as f:
 
         # basename
         basename = os.path.basename(path)
         stem, extention = os.path.splitext(basename)
-        if extention.lower() != '.kif':
+        if extention.lower() != '.kifu':
             return ""
 
         # Append new extention
-        basename = f"{stem}.kifu"
+        basename = f"{stem}.kif"
 
-        newPath = os.path.join('kifu', basename)
+        newPath = os.path.join('kif', basename)
 
-        with codecs.open(newPath, "w", encoding='utf-8') as fOut:
+        with codecs.open(newPath, "w", encoding='shift_jis') as fOut:
 
             # UTF-8形式に変換して保存
             for row in f:
@@ -39,16 +39,16 @@ def convert_kif_file(path):
 
     # with句を抜けて、ファイルを閉じたあと
     # ファイルの移動
-    new_path = shutil.move(path, 'kif-done')
+    new_path = shutil.move(path, 'kifu-done')
     print(new_path)
 
     return newPath
 
 def main():
     # KIFファイル一覧
-    files = glob.glob("./kif/*")
+    files = glob.glob("./kifu/*")
     for file in files:
-        _newPath = convert_kif_file(file)
+        _newPath = convert_kifu_file(file)
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
