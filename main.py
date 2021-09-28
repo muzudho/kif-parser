acbeba8e0b427b69647b11b532dea7f611eb8a94
むずでょ <muzudho1@gmail.com>
2021-09-28 19:26:33 +0900

    ファイルの移動

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import glob
+import shutil
+import os
 
-# ファイル一覧
-files = glob.glob("./input/*")
-for file in files:
-    print(file)
+def read_kif_file(path):
+    """KIFファイルを読み取ります
+    """
+    
+    success = False
+    
+    # シフトJISエンコードのテキストファイルの読み込み
+    with open(path, encoding='shift_jis') as f:
+        s = f.read()
+
+        print(s.rstrip())  # このファイルはシフトJISでエンコードされています
+        
+        # KIFU形式に変換
+        
+        success = True
+
+    if success:
+        # ファイルの移動
+        new_path = shutil.move(path, 'input-done')
+        print(new_path)
+    
+def main():
+    # ファイル一覧
+    files = glob.glob("./input/*")
+    for file in files:
+        read_kif_file(file)
+
+# このファイルを直接実行したときは、以下の関数を呼び出します
+if __name__ == "__main__":
+    main()
