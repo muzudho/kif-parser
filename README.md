2fbd5b726cfb2c18eb57f5fc377dd6b89c6bd376
むずでょ <muzudho1@gmail.com>
2021-09-28 21:12:30 +0900

    kifu-to-pibot

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ WIP KIFパーサー
 2. ターミナルで `python.exe kifu-to-kif.py` コマンドを実行してください
 3. Shift-JIS形式に変換して 📂`kif` ファイルに変換されます
 4. 読み終えた `kifu/*.kifu` ファイルは、 `kifu-done/*.kifu` へ移動されます
+
+## KIFU から PIBOT へ変換
+
+1. 📂`kifu` に `*.kifu` ファイルをたくさん入れてください
+2. ターミナルで `python.exe kifu-to-pibot.py` コマンドを実行してください
+3. JSON形式に変換して 📂`pibot` ファイルに変換されます
+4. 読み終えた `kifu/*.kifu` ファイルは、 `kifu-done/*.kifu` へ移動されます
--- kifu-to-pibot.py
+++ kifu-to-pibot.py
@@ -1,22 +1,31 @@
 import glob
 import re
 import os
+import json
 
 __handicap = re.compile(r"^手合割：(.+)$")
 
 def main():
+
+
     # KIFUファイル一覧
     files = glob.glob("./kifu/*")
     for file in files:
+        data = {}
 
         # basename
         basename = os.path.basename(file)
-        _stem, extention = os.path.splitext(basename)
+        stem, extention = os.path.splitext(basename)
         if extention.lower() != '.kifu':
             return
 
+        # insert new extention
+        newPath = os.path.join('./pibot', f"{stem}.json")
+
         # とりあえず KIFU を読んでみます
+        rowNumber = 1
         with open(file, encoding='utf-8') as f:
+
             s = f.read()
             text = s.rstrip()
 
@@ -27,44 +36,48 @@ def main():
                 if result:
                     handicap = result.group(1)
                     if handicap == '平手':
-                        print("★平手 WIP")
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Hirate"}
                     elif handicap == '香落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostLance"}
                     elif handicap == '右香落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostRightLance"}
                     elif handicap == '角落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostBishop"}
                     elif handicap == '飛車落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostRook"}
                     elif handicap == '飛香落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostRookLance"}
                     elif handicap == '二枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost2Pieces"}
                     elif handicap == '三枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost3Pieces"}
                     elif handicap == '四枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost4Pieces"}
                     elif handicap == '五枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost5Pieces"}
                     elif handicap == '左五枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostLeft5Pieces"}
                     elif handicap == '六枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost6Pieces"}
                     elif handicap == '左七枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostLeft7Pieces"}
                     elif handicap == '右七枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"LostRight7Pieces"}
                     elif handicap == '八枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost8Pieces"}
                     elif handicap == '十枚落ち':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Lost10Pieces"}
                     elif handicap == 'その他':
-                        pass
+                        data[f'{rowNumber}'] = {"Handicap":"Other"}
 
+                    rowNumber += 1
                     continue
 
-                print(f"> [{line}]")
+                data[f'{rowNumber}'] = {"Line":f"{line}"}
+                rowNumber += 1
+
+        with open(newPath, 'w', encoding='utf-8') as fOut:
+            fOut.write(json.dumps(data, indent=4))
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
