09b5f27c70e65457976419035f4130dde3f24554
むずでょ <muzudho1@gmail.com>
2021-09-28 20:01:27 +0900

    KIFU パース中

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ WIP KIFパーサー
 
 ## 使い方
 
-1. 📂`input` に `*.KIF` ファイルを入れてください  
-2. ターミナルで `python.exe main.py` コマンドを実行してください  
-3. 
+1. 📂`input` に `*.KIF` ファイルを入れてください
+2. ターミナルで `python.exe main.py` コマンドを実行してください
+3. 読込んだ `input/*.KIF` ファイルは、 `input-done/*.KIF` へ移動されます
+4. UTF-8形式に変換して `hidden-step1/*.kifu` ファイルに変換されます
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import shutil
 import os
 import codecs
 import sys
+import re
+
+__handicap = re.compile(r"^手合割：(.+)$")
 
 def read_kif_file(path):
     """(1) input フォルダーのKIFファイルを読み取ります
@@ -55,7 +58,53 @@ def main():
         # とりあえず KIFU を読んでみます
         with open(file, encoding='utf-8') as f:
             s = f.read()
-            print(s.rstrip())  # このファイルはシフトJISでエンコードされています
+            text = s.rstrip()
+
+            lines = text.split('\n')
+            for line in lines:
+
+                result = __handicap.match(line)
+                if result:
+                    handicap = result.group(1)
+                    if handicap == '平手':
+                        print("★平手 WIP")
+                        pass
+                    elif handicap == '香落ち':
+                        pass
+                    elif handicap == '右香落ち':
+                        pass
+                    elif handicap == '角落ち':
+                        pass
+                    elif handicap == '飛車落ち':
+                        pass
+                    elif handicap == '飛香落ち':
+                        pass
+                    elif handicap == '二枚落ち':
+                        pass
+                    elif handicap == '三枚落ち':
+                        pass
+                    elif handicap == '四枚落ち':
+                        pass
+                    elif handicap == '五枚落ち':
+                        pass
+                    elif handicap == '左五枚落ち':
+                        pass
+                    elif handicap == '六枚落ち':
+                        pass
+                    elif handicap == '左七枚落ち':
+                        pass
+                    elif handicap == '右七枚落ち':
+                        pass
+                    elif handicap == '八枚落ち':
+                        pass
+                    elif handicap == '十枚落ち':
+                        pass
+                    elif handicap == 'その他':
+                        pass
+
+                    continue
+
+                print(f"> [{line}]")
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
