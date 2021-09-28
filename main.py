3a4979abd41f750d02e5cc1d147175a51f59c3ff
むずでょ <muzudho1@gmail.com>
2021-09-28 19:36:10 +0900

    kifuファイルへ一旦変換

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import glob
 import shutil
 import os
+import codecs
+import sys
 
 def read_kif_file(path):
-    """KIFファイルを読み取ります
+    """(1) input フォルダーのKIFファイルを読み取ります
+    (2) KIFUファイルを hidden-step1 フォルダーへ生成します
+    (3) KIFファイルは input-done フォルダーへ移動します
     """
-    
-    success = False
-    
-    # シフトJISエンコードのテキストファイルの読み込み
-    with open(path, encoding='shift_jis') as f:
-        s = f.read()
 
-        print(s.rstrip())  # このファイルはシフトJISでエンコードされています
-        
-        # KIFU形式に変換
-        
+    success = False
+
+    # シフトJISエンコードのテキストファイルの読み込み
+    with codecs.open(path, "r", encoding='shift_jis') as f:
+
+        # basename
+        basename = os.path.basename(path)
+        # Except old extention
+        stem = os.path.splitext(basename)[0]
+        # Append new extention
+        basename = f"{stem}.kifu"
+
+        destination = os.path.join('hidden-step1', basename)
+
+        with codecs.open(destination, "w", encoding='utf-8') as fOut:
+
+            # UTF-8形式に変換して保存
+            for row in f:
+                fOut.write(row)
+
         success = True
+
+    # with句を抜けて、ファイルを閉じたあと
 
     if success:
         # ファイルの移動
         new_path = shutil.move(path, 'input-done')
         print(new_path)
-    
+
 def main():
     # ファイル一覧
     files = glob.glob("./input/*")
