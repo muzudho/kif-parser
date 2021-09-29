d3f29747723f5b78a5f9139ce583d1b4e0cb602c
むずでょ <muzudho1@gmail.com>
2021-09-29 19:18:00 +0900

    test

--- kif_to_pibot.py
+++ kif_to_pibot.py
@@ -1,16 +1,23 @@
 import glob
-from kifu_to_pibot import parse_kifu_file_to_pibot
+from kifu_to_pibot import convert_kifu_file_to_pibot
 from kif_to_kifu import convert_kif_to_kifu_file
+
+def convert_kif_to_pibot(kifFile):
+    kifuFile, doneKifFile = convert_kif_to_kifu_file(kifFile)
+    print(f'kifu={kifuFile}')
+
+    if kifuFile:
+        pibotFile, _doneKifuFile = convert_kifu_file_to_pibot(kifuFile)
+        print(f'kifu={kifuFile} pibot={pibotFile}')
+        return pibotFile, doneKifFile
+
+    return None, None, None
 
 def main():
     # KIFファイル一覧
-    files = glob.glob("./kif/*")
-    for file in files:
-        outPath, _donePath = convert_kif_to_kifu_file(file)
-        print(f'kifu={outPath}')
-
-        if outPath:
-            _outPath, _donePath = parse_kifu_file_to_pibot(outPath)
+    kifFiles = glob.glob("./kif/*")
+    for kifFile in kifFiles:
+        _pibotFile, _doneKifFile = convert_kif_to_pibot(kifFile)
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
--- test_kif.py
+++ test_kif.py
@@ -1,20 +1,26 @@
 import glob
 from scripts.test_lib import create_sha256
+from kif_to_pibot import convert_kif_to_pibot
 
 def main():
     # KIFファイル一覧
-    files = glob.glob("./kif/*")
-    for file in files:
+    kifFiles = glob.glob("./kif/*")
+    for kifFile in kifFiles:
 
         # 読み取り専用、バイナリ
-        with open(file, 'rb') as f:
-            binaryData = f.read()
+        with open(kifFile, 'rb') as f:
+            kifBinary = f.read()
 
             # print(binaryData)
 
             # ファイルをバイナリ形式で読み込んで SHA256 生成
-            sha256 = create_sha256(binaryData)
-            print(f"sha256={sha256}")
+            kifSha256 = create_sha256(kifBinary)
+            print(f"kifSha256={kifSha256}")
+
+            # kif -> pibot 変換
+            _pibotFile, _doneKifFile = convert_kif_to_pibot(kifFile)
+
+            # TODO pibot -> kif 変換
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
 if __name__ == "__main__":
