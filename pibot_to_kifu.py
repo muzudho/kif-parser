b3e3a154bababbd756d7975d2bfa73e55884e338
むずでょ <muzudho1@gmail.com>
2021-09-29 20:06:33 +0900

    WIP pibot to kifu

--- pibot_to_kifu.py
+++ pibot_to_kifu.py
@@ -22,14 +22,18 @@ def convert_pibot_to_kifu(pibotFile):
 
         # TODO JSON to KIFU
         for rowNumber, rowData in data.items():
-            print(f"krowNumberey={rowNumber} rowData={rowData}")
-            kifu_text += f"krowNumberey={rowNumber} rowData={rowData}\n"
+
+            if rowData["Type"] == "Comment":
+                kifu_text += f"#{rowData['Comment']}\n"
+            else:
+                print(f"krowNumberey={rowNumber} rowData={rowData}")
+                kifu_text += f"krowNumberey={rowNumber} rowData={rowData}\n"
 
         # New .kifu ファイル出力
         kifuFile = os.path.join('kifu', f"{stem}.kifu")
         with open(kifuFile, mode='w', encoding='utf-8') as fOut:
-            # fOut.write(kifu_text)
-            pass
+            print(f"kifu_text={kifu_text}")
+            fOut.write(kifu_text)
 
     # with句を抜けて、ファイルを閉じたあと
     # ファイルの移動
