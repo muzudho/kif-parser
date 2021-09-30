9fdbc457804bef0a540f3fd003193dff1fbaf8eb
むずでょ <muzudho1@gmail.com>
2021-09-30 23:45:48 +0900

    Create undo_kifu.py

---
+++ undo_kifu.py
@@ -0,0 +1,27 @@
+import glob
+import shutil
+import os
+
+
+def undo_kifu(kifu_file):
+    # basename
+    basename = os.path.basename(kifu_file)
+    _stem, extention = os.path.splitext(basename)
+    if extention.lower() != '.kifu':
+        return None
+
+    # kifuフォルダーへ移動します
+    undone_kifu_file = shutil.move(kifu_file, os.path.join('kifu', basename))
+    return undone_kifu_file
+
+
+def main():
+    # KIFUファイル一覧
+    kifu_files = glob.glob("./kifu-done/*.kifu")
+    for kifu_file in kifu_files:
+        _undone_kifu_file = undo_kifu(kifu_file)
+
+
+# このファイルを直接実行したときは、以下の関数を呼び出します
+if __name__ == "__main__":
+    main()
