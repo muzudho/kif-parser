530b5bb6fe58eca26f7d119878619fe42a8dea01
むずでょ <muzudho1@gmail.com>
2021-10-02 11:50:22 +0900

    Update pivot_to_toml.py

--- pivot_to_toml.py
+++ pivot_to_toml.py
@@ -29,74 +29,150 @@ def convert_pivot_to_toml(pivot_file, output_folder='temporary/toml', done_folde
     with open(pivot_file, encoding='utf-8') as f:
         data = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
-        move_section_flag = False
         toml_text = ""
+        buffer = ""
+
+        # 棋譜は 文書構造であり 意味でまとまってないので（コメントはどこにでもある）
+        # それでは不便なので ある程度の区画にまとめます
+        section_number = 1
+        pre_section_type = ""
 
         # JSON to TOML
-        for rowNumber, rowData in data.items():
+        for row_number, row_data in data.items():
 
-            if rowData["type"] == "Comment":
-                comment = rowData["comment"]
+            row_type = row_data["type"]
+
+            if row_type == "Comment":
+                # コメントは（キーが無いので重複してしまうのでそれを避けるために） 1 行で 1 セクション使います
+                toml_text += buffer  # flush
+                buffer = ""
+
+                comment = row_data["comment"]
+
                 # TOMLのコメントにすると、パーサーがコメントを読み込んでくれないので、
                 # コメントにはしません
                 # TODO 文字列エスケープどうする？
-                # TODO コメントが複数あることに対応できない（名前の重複）
-                toml_text += f"comment='''{comment}'''\n"
-            elif rowData["type"] == "Explanation":
-                explanation = rowData["explanation"]
+                toml_text += f"""[[comment.{section_number}]]
+comment='''{comment}'''
+"""
+
+                section_number += 1
+                pre_section_type = row_type
+
+            elif row_type == "Explanation":
+                # 指し手等へのコメントは（キーが無いので重複してしまうのでそれを避けるために） 1 行で 1 セクション使います
+                toml_text += buffer
+                buffer = ""
+
+                explanation = row_data["explanation"]
+
                 # TODO 文字列エスケープどうする？
-                toml_text += f"explanation='''{explanation}'''\n"
-            elif rowData["type"] == "Bookmark":
-                bookmark = rowData["bookmark"]
+                toml_text += f"""[[explanation.{section_number}]]
+explanation='''{explanation}'''
+"""
+
+                section_number += 1
+                pre_section_type = row_type
+
+            elif row_type == "Bookmark":
+                # しおりは（キーが無いので重複してしまうのでそれを避けるために） 1 行で 1 セクション使います
+                toml_text += buffer
+                buffer = ""
+
+                bookmark = row_data["bookmark"]
+
                 # TODO 文字列エスケープどうする？
-                toml_text += f"bookmark='''{bookmark}'''\n"
-            elif rowData["type"] == "Move":
+                toml_text += f"""[[bookmark.{section_number}]]
+bookmark='''{bookmark}'''
+"""
+
+                section_number += 1
+                pre_section_type = row_type
+
+            elif row_type == "Move":
                 # 指し手
+                if pre_section_type != row_type:
+                    toml_text += buffer
+                    buffer = f"""[[moves.{section_number}]]
+"""
+                    section_number += 1
 
-                if not move_section_flag:
-                    toml_text += "[moves]\n"
-                    move_section_flag = True
+                buffer += move_statement_p.from_pivot(
+                    moves=row_data["moves"],
+                    elapsedTime=row_data["elapsedTime"],
+                    totalElapsedTime=row_data["totalElapsedTime"],
+                    move=row_data["move"])
 
-                toml_text += move_statement_p.from_pivot(
-                    moves=rowData["moves"],
-                    elapsedTime=rowData["elapsedTime"],
-                    totalElapsedTime=rowData["totalElapsedTime"],
-                    move=rowData["move"])
+                pre_section_type = row_type
 
-            elif rowData["type"] == "Handicap":
-                handicap = handicap_p.from_pivot(rowData["handicap"])
-                toml_text += f"handicap='{handicap}'\n"
-            elif rowData["type"] == "Player":
+            elif row_type == "Handicap":
+                if pre_section_type != row_type:
+                    toml_text += buffer
+                    buffer = f"""[[handicap.{section_number}]]
+"""
+                    section_number += 1
+
+                handicap = handicap_p.from_pivot(row_data["handicap"])
+                buffer += f"handicap='{handicap}'\n"
+
+                pre_section_type = row_type
+
+            elif row_type == "Player":
+                if pre_section_type != row_type:
+                    toml_text += buffer
+                    buffer = f"""[[player.{section_number}]]
+"""
+                    section_number += 1
+
                 player_phase = player_phase_p.from_pivot(
-                    rowData["playerPhase"])
-                player_name = rowData["playerName"]
-                toml_text += f"{player_phase}='''{player_name}'''\n"
-            elif rowData["type"] == "Result":
-                if "reason" in rowData:
+                    row_data["playerPhase"])
+
+                player_name = row_data["playerName"]
+
+                buffer += f"""{player_phase}='''{player_name}'''
+"""
+
+                pre_section_type = row_type
+
+            elif row_type == "Result":
+                if pre_section_type != row_type:
+                    toml_text += buffer
+                    buffer = f"""[[result.{section_number}]]
+"""
+                    section_number += 1
+
+                if "reason" in row_data:
                     # Example: `まで52手で時間切れにより後手の勝ち`
-                    moves = rowData["moves"]
-                    reason = rowData['reason']
-                    winner = rowData["winner"]
-                    judge = rowData["judge"]
-                    toml_text += judge_statement3_p.from_pivot(
+                    moves = row_data["moves"]
+                    reason = row_data['reason']
+                    winner = row_data["winner"]
+                    judge = row_data["judge"]
+                    buffer += judge_statement3_p.from_pivot(
                         moves, reason, winner, judge)
-                elif "winner" in rowData:
+                elif "winner" in row_data:
                     # Example: `まで64手で後手の勝ち`
-                    moves = rowData["moves"]
-                    winner = rowData["winner"]
-                    judge = rowData["judge"]
-                    toml_text += judge_statement1_p.from_pivot(
+                    moves = row_data["moves"]
+                    winner = row_data["winner"]
+                    judge = row_data["judge"]
+                    buffer += judge_statement1_p.from_pivot(
                         moves, winner, judge)
                 else:
                     # Example: `まで63手で中断`
-                    moves = rowData["moves"]
-                    judge = rowData["judge"]
-                    toml_text += judge_statement2_p.from_pivot(
+                    moves = row_data["moves"]
+                    judge = row_data["judge"]
+                    buffer += judge_statement2_p.from_pivot(
                         moves, judge)
+
+                pre_section_type = row_type
+
             else:
                 # Error
-                print(f"Error: rowNumberey={rowNumber} rowData={rowData}")
+                print(f"Error: rowNumberey={row_number} row_data={row_data}")
                 return None, None
+
+        if buffer != "":
+            toml_file += buffer
+            buffer = ""
 
         # New .kifu ファイル出力
         toml_file = os.path.join(output_folder, f"{stem}.toml")
