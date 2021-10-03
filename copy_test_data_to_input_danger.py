457c6c69e4a5716bde9564d737da05c46f7e515b
むずでょ <muzudho1@gmail.com>
2021-10-03 19:04:29 +0900

    pattern list to pattern

--- copy_test_data_to_input_danger.py
+++ copy_test_data_to_input_danger.py
@@ -1,5 +1,5 @@
 import glob
-from scripts.copy_files_to_one_folder_by_pattern_list import copy_files_to_one_folder_by_pattern_list
+from scripts.copy_files_to_one_folder_by_pattern import copy_files_to_one_folder_by_pattern
 
 
 def main():
@@ -8,8 +8,9 @@ def main():
                                    './test-data/*.json',
                                    './test-data/*.toml')
 
-    copy_files_to_one_folder_by_pattern_list(
-        test_data_file_pattern_list, output_folder='input')
+    for test_data_file_pattern in test_data_file_pattern_list:
+        copy_files_to_one_folder_by_pattern(
+            test_data_file_pattern, output_folder='input')
 
 
 # このファイルを直接実行したときは、以下の関数を呼び出します
---
+++ scripts/copy_files_to_one_folder_by_pattern.py
@@ -0,0 +1,28 @@
+import glob
+import shutil
+import os
+import sys
+
+
+def copy_files_to_one_folder_by_pattern(file_pattern, destination_folder):
+
+    # 出力ファイル一覧
+    file_list = glob.glob(file_pattern)
+    for file in file_list:
+        # basename
+        try:
+            basename = os.path.basename(file)
+        except:
+            print(
+                f"Error: file={file} except={sys.exc_info()[0]}")
+            raise
+
+        # `.` や `*` や `/` がここを通ってきてないか、最終チェックしておきます
+        _stem, extention = os.path.splitext(basename)
+        if not (extention.lower() in ['.kif', '.kifu', '.json', '.toml']):
+            print(
+                f"Error: record file move fail. copy_files_to_one_folder_by_pattern.py file=[{file}] file_pattern=[{file_pattern}]")
+            continue
+
+        shutil.copyfile(
+            file, os.path.join(destination_folder, basename))
