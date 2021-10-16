25123fca7b31bbd01b3fa17254cb726c71c25808
むずでょ <muzudho1@gmail.com>
2021-10-17 03:50:19 +0900

    ループ減らす

--- translate.py
+++ translate.py
@@ -11,6 +11,7 @@ def translate(source, destination, template, debug):
     if debug:
         print(f"[DEBUG] translate.py translate(): to_pivot")
 
+    # to-pivot
     if source == 'kifu':
         # KIFUファイルをPIVOTへ変換します
         to_pivot = ReversibleConvertKifuToPivot(
@@ -20,19 +21,7 @@ def translate(source, destination, template, debug):
         to_pivot = ReversibleConvertKifToPivot(
             debug=debug, last_layer_folder='temporary/output-pivot', no_remove_output_pivot=True, template_name=template)
 
-    to_pivot.clean_last_layer_folder()
-
-    for input_file in to_pivot.outside_input_files():
-        copy = change_place(to_pivot.layer2_folder, input_file)
-        copy_file(input_file, copy, debug=debug)
-
-    for input_file in to_pivot.target_files():
-        to_pivot.round_trip_translate(input_file)
-    to_pivot.clean_temporary()
-
-    if debug:
-        print(f"[DEBUG] translate.py translate(): from_pivot")
-
+    # from-pivot
     if destination == 'kifu':
         # PIVOTファイルをKIFUへ変換します
         from_pivot = ReversibleConvertPivotToKifu(
@@ -41,6 +30,23 @@ def translate(source, destination, template, debug):
         # PIVOTファイルをKIFへ変換します
         from_pivot = ReversibleConvertPivotToKif(
             debug=debug, first_layer_folder='temporary/output-pivot', template_name=template)
+
+    # 最終フォルダーを掃除
+    to_pivot.clean_last_layer_folder()
+
+    for input_file in to_pivot.outside_input_files():
+        # 入力フォルダ―にあるファイルを、レイヤー２フォルダーにコピーします
+        next_file = change_place(to_pivot.layer2_folder, input_file)
+        copy_file(input_file, next_file, debug=debug)
+
+        # レイヤー２フォルダーにあるファイルを往復翻訳します
+        to_pivot.round_trip_translate(next_file)
+
+    # 一時フォルダーを掃除
+    to_pivot.clean_temporary()
+
+    if debug:
+        print(f"[DEBUG] translate.py translate(): from_pivot")
 
     from_pivot.clean_last_layer_folder()
 
