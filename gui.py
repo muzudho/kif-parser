d708b2c7dd58b46bfb3498b54c65a0db7abba9f9
むずでょ <muzudho1@gmail.com>
2021-10-17 23:36:55 +0900

    fix gui.py

--- gui.py
+++ gui.py
@@ -9,7 +9,8 @@ from scripts.generator_identification import GeneratorIdentification
 def __main():
     global left_generator_combobox_value, right_generator_combobox_value
     global left_stem_text_box_value
-    global left_encoding_combobox_value, right_encoding_combobox_value
+    global left_encoding_combobox_value
+    global right_encoding_combobox_value
     global left_file_text_box_value, left_file_text_box, right_file_text_box_value, right_file_text_box
     global left_text_area, right_text_area
 
@@ -54,11 +55,18 @@ def __main():
 
     window = tk.Tk()
     window.title(u"Tkinter practice")
-    # window.geometry("480x360")
-    window.geometry("960x720")
+    window.geometry("960x720")  # 2倍のスケール
+
+    # 値を前もって用意（再計算が先に働いてしまう前に）
+    left_generator_combobox_value = tk.StringVar()
+    right_generator_combobox_value = tk.StringVar()
+    left_stem_text_box_value = tk.StringVar()
+    left_encoding_combobox_value = tk.StringVar()
+    right_encoding_combobox_value = tk.StringVar()
+    left_file_text_box_value = tk.StringVar()
+    right_file_text_box_value = tk.StringVar()
 
     # 左コンボボックス（ジェネレーター名）
-    left_generator_combobox_value = tk.StringVar()
     left_generator_combobox_value.set("Shogi GUI")
     left_generator_combobox = ttk.Combobox(
         window, height=3, textvariable=left_generator_combobox_value, values=generator_name_list)
@@ -68,7 +76,6 @@ def __main():
         '<<ComboboxSelected>>', left_generator_combobox_selected)
 
     # 右コンボボックス（ジェネレーター名）
-    right_generator_combobox_value = tk.StringVar()
     right_generator_combobox_value.set("Shogi-dokoro")
     right_generator_combobox = ttk.Combobox(
         window, height=3, textvariable=right_generator_combobox_value, values=generator_name_list)
@@ -78,7 +85,6 @@ def __main():
         '<<ComboboxSelected>>', right_generator_combobox_selected)
 
     # 左テキストボックス1（ファイル名基幹部）
-    left_stem_text_box_value = tk.StringVar()
     left_stem_text_box_value.trace("w", on_left_stem_text_box_value_changed)
     left_stem_text_box = tk.Entry(
         window, textvariable=left_stem_text_box_value)
@@ -87,7 +93,6 @@ def __main():
     left_stem_text_box.insert(tk.END, "demo")
 
     # 左コンボボックス（エンコーディング）
-    left_encoding_combobox_value = tk.StringVar()
     left_encoding_combobox_value.set("KIFU (UTF-8)")
     left_encoding_combobox = ttk.Combobox(
         window, height=3, textvariable=left_encoding_combobox_value, values=encoding_name_list)
@@ -97,7 +102,6 @@ def __main():
         '<<ComboboxSelected>>', left_encoding_combobox_selected)
 
     # 右コンボボックス（エンコーディング）
-    right_encoding_combobox_value = tk.StringVar()
     right_encoding_combobox_value.set("KIFU (UTF-8)")
     right_encoding_combobox = ttk.Combobox(
         window, height=3, textvariable=right_encoding_combobox_value, values=encoding_name_list)
@@ -107,7 +111,6 @@ def __main():
         '<<ComboboxSelected>>', right_encoding_combobox_selected)
 
     # 左テキストボックス2（ファイル名）
-    left_file_text_box_value = tk.StringVar()
     left_file_text_box = tk.Entry(
         window, textvariable=left_file_text_box_value)
     left_file_text_box.place(x=10*scale, y=70*scale,
@@ -116,7 +119,6 @@ def __main():
     left_file_text_box.configure(state='readonly')
 
     # 右テキストボックス2（ファイル名）
-    right_file_text_box_value = tk.StringVar()
     right_file_text_box = tk.Entry(
         window, textvariable=right_file_text_box_value)
     right_file_text_box.place(x=270*scale, y=70*scale,
@@ -183,15 +185,19 @@ def right_encoding_combobox_selected(e):
 
 
 def create_left_file_name():
-    global left_generator_combobox_value, left_encoding_combobox_value
+    global left_generator_combobox_value
+    global left_encoding_combobox_value
+
     filename = "input/"
     stem = left_stem_text_box_value.get()
     filename += stem
+
     generator = left_generator_combobox_value.get()
     if generator == "Shogi GUI":
         filename += "[shogigui]"
     else:
         filename += "[shogidokoro]"
+
     encoding = left_encoding_combobox_value.get()
     if encoding == "KIF (Shift-JIS)":
         filename += ".kif"
@@ -222,6 +228,8 @@ def recognition():
     """TODO フォーマット認識ボタン"""
     global left_generator_combobox_value
     global left_text_area
+    global left_file_text_box_value
+    global right_file_text_box_value
     left_textbox_content = left_text_area.get("1.0", 'end-1c')
     generator_identification = GeneratorIdentification()
     generator_identification.read_all_text(left_textbox_content)
@@ -238,10 +246,17 @@ def recognition():
     print(
         f'[DEBUG] best_key={best_key} best_value={best_value}')
 
+    # 再計算は自動でしてくれないみたいだ
     if best_key == "shogigui":
         left_generator_combobox_value.set("Shogi GUI")
     else:
         left_generator_combobox_value.set("Shogi-dokoro")
+
+    # 再計算
+    left_filename = create_left_file_name()
+    left_file_text_box_value.set(left_filename)
+    right_filename = create_right_file_name()
+    right_file_text_box_value.set(right_filename)
 
 
 def copy_left_to_right():
